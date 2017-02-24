25ba79b59520283a575e83f8268c586056efc080
dogoncouch <dpersonsdev@gmail.com>
2017-02-24 01:14:03 -0500

    Finished license object template

--- licins/licenses/type.py
+++ licins/licenses/type.py
@@ -26,23 +26,44 @@ class LicenseData:
     def __init__(self, options):
         import os
         """Initialize a license object"""
-        self.name = ""
-        self.desc = ""
-        self.header = ""
-        self.full = ""
+        self.name = ''
+        self.desc = ''
+        # Short header version:
+        self.header = ''
+        # Full version:
+        self.full = ''
+        # What comes before your name on copyright line:
+        self.copyrightpre = ''
+        self.finalproduct = ''
         pass
 
-    def prep(self, instype = 'header', prefix = '', encoding = '', \
-            signature = ''):
+    def prep(self, instype = 'header', comment = '# ', cname = None, \
+            progdesc = None, prefix = None, encoding = None, signature = ''):
         """Prepare the license with comments, etc."""
-        pass
+        if instype = 'header': work = self.header.split('\n')
+        if instype = 'full': work = self.full.split('\n')
+        # Comment all of the lines
+        for line in work:
+            line = comment + line
+        # Add all of the lines in reverse order:
+        if cname and signature:
+            copyright = self.copyrightpre + cname + '\n' + signature
+            work.insert(0, copyright)
+        if progdesc: work.insert(0, progdesc)
+        if encoding: work.insert(0, encoding)
+        if prefix: work.insert(0, prefix)
+        # Prepare the final product:
+        self.finalproduct = '\n'.join(work)
 
-    def write(self, inputfile, printlicense):
+    def write(self, inputfile):
         """Insert the formatted license into a file"""
-        lines = os.popen(inputfile).readlines()
+        lines = open(inputfile, 'r').readlines()
+        # Ignore the first line if it starts with #!:
         if lines[0].startswith('#!'): startline = 1
         else: startline = 0
-        final = lines.insert(startline, printlicense)
+        # Read the file and add our prepared license:
+        final = lines.insert(startline, self.finalproduct)
+        # Write the final result:
         output = open(inputfile, 'w')
         output.write(final)
         output.close()
