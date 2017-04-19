a339cef4056b275b2c9902222ab49e1de6adc3f9
dogoncouch <dpersonsdev@gmail.com>
2017-04-19 16:41:32 -0400

    Update __init__.py

--- LicIns/__init__.py
+++ LicIns/__init__.py
@@ -2,4 +2,4 @@ __version__ = '1.0'
 __author__ = 'Dan Persons <dpersonsdev@gmail.com>'
 __license__ = 'MIT License'
 __github__ = 'https://github.com/dogoncouch/licins'
-__all__ = 'core'
+__all__ = ['core']
